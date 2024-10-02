844f077d53516333c3accd784dabe5c18633a184
lineulimasjc <lineulimasjc@gmail.com>
2024-10-02 18:37:12 -0300

    Adding images

--- pages/01_Introdução.py
+++ pages/01_Introdução.py
@@ -8,7 +8,51 @@ st.set_page_config(
     initial_sidebar_state="auto",
 )
 
-st.title('Introdução')
+st.header('Programação Estruturada vs Orientada a Objetos')
+
+st.subheader("1. Programação Estruturada")
+
+st.write("**Programação Estruturada** é um método de desenvolvimento de programas. Este método usa três tipos de estruturas de controle.")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("Sequência")
+    st.write("As instruções são executas de forma sequencial.")
+    
+with col2:
+    st.header("Seleção")
+    st.write("As instruções são executadas com base em uma ou mais condições.")
+
+with col3:
+    st.header("Repetição")
+    st.write("As instruções são executadas enquanto a condição for satisfeita.")
+
+st.write("###")
+
+st.write("Exemplos:")
+
+st.markdown("- C")
+st.markdown("- C++")
+st.markdown("- Pascal")
+st.markdown("- Rust")
+
+st.divider()
+
+
+
+
+st.subheader("2. Programação Orientada a Objetos")
+
+st.write("**Programação Orientada a Objetos** é um método de desenvolvimento de programas. Este método é baseado no conceito de “**objetos**“. Os objetos podem conter **dados** e **códigos**.")
+
+st.write("###")
+
+st.write("Exemplos:")
+
+st.markdown("- C++")
+st.markdown("- Python")
+st.markdown("- Java")
 
 
 
