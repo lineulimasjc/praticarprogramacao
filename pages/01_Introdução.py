512eceef19e995939a260099626c42e098a02c13
lineulimasjc <lineulimasjc@gmail.com>
2024-10-02 20:11:23 -0300

    Update content.

--- pages/01_Introdução.py
+++ pages/01_Introdução.py
@@ -3,15 +3,61 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Introdução",
-    page_icon="images/logo.png",
+    page_icon="images\logo.png",
     layout="centered",
     initial_sidebar_state="auto",
 )
 
-st.title('Introdução')
+st.header('Programação Estruturada vs Orientada a Objetos')
+
+st.subheader("1. Programação Estruturada")
+
+st.write("**Programação Estruturada** é um método de desenvolvimento de programas. Este método usa três tipos de estruturas de controle.")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("Sequência")
+    st.write("As instruções são executas de forma sequencial.")
+    st.image("images/sequencia.svg")
+    
+with col2:
+    st.header("Seleção")
+    st.write("As instruções são executadas com base em uma ou mais condições.")
+    st.image("images/selecao.svg")
+
+with col3:
+    st.header("Repetição")
+    st.write("As instruções são executadas enquanto a condição for satisfeita.")
+    st.image("images/repeticao.svg")
+
+st.write("###")
+
+st.write("Exemplos:")
+
+st.markdown("- C")
+st.markdown("- C++")
+st.markdown("- Pascal")
+st.markdown("- Rust")
+
+st.divider()
 
 
 
+
+st.subheader("2. Programação Orientada a Objetos")
+
+st.write("**Programação Orientada a Objetos** é um método de desenvolvimento de programas. Este método é baseado no conceito de “**objetos**“. Os objetos podem conter **dados** e **códigos**.")
+
+st.image("images/orientacao-a-objetos.svg")
+
+st.write("###")
+
+st.write("Exemplos:")
+
+st.markdown("- C++")
+st.markdown("- Python")
+st.markdown("- Java")
 
 
 
