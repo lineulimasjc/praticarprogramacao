93f620e0d56ecaf60bb5cd56c88916c2d28fd208
lineulimasjc <lineulimasjc@gmail.com>
2024-10-03 18:43:54 -0300

    Adding content.

--- pages/06_Laço_de_Repetição.py
+++ pages/06_Laço_de_Repetição.py
@@ -11,8 +11,9 @@ st.set_page_config(
 
 st.title("Laço de Repetição")
 
-t1, t2, t3, t4 = st.tabs(["Operadores de Incremento e Decremento", "```while```", "```do...while```", "```for```"])
+t1, t2, t3, t4 = st.tabs(["Operadores", "```while```", "```do...while```", "```for```"])
 
+t1.subheader("Operadores de Incremento e Decremento")
 
 t1.markdown("""
 | Operador    | Exemplo     | Explicação                                                     |
@@ -136,8 +137,6 @@ t1.code(code, language="cpp")
 
 t1.write('Saída:')
 
-
-
 t1.text('''
 Contador = 1
 Contador = 1
@@ -145,6 +144,153 @@ Contador = 1
 
 
 # Adicionar o aqui aqui
+
+
+
+
+
+# while tab
+
+t2.subheader("While")
+
+t2.write("O comando ```while``` executa a repetição de um bloco de instruções enquanto a condição é verdadeira.")
+
+t2.write("Sintaxe")
+
+t2.text('''
+while (condição)
+{
+  Instrução ou bloco de instruções;
+}
+''')
+
+t2.subheader("Exemplo 1")
+
+t2.write("Ao exercutar o código abaixo, será impresso na tela os números de 0 a 9.")
+
+t2.write("A variável ```i``` na instrução ```i = i + 1``` é um contador, que funciona como um acumulador ou somatório. O contador é usado para controlar o número de vezes que o laço repetirá, sendo incrementado em 1 a cada execução do laço.")
+
+
+code = '''
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int i = 0;
+
+  while (i < 10)
+  {
+    cout << i << endl;
+
+    i = i + 1;
+  }
+}
+'''
+t2.code(code, language="cpp")
+
+t2.write('Saída:')
+
+t2.text('''
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+''')
+
+
+
+t2.subheader("Exemplo 2")
+
+t2.write("Neste exemplo, o usuário poderá ***entrar com os dados** e, quando desejar **finalizar o programa**, deverá **digitar -1**.")
+
+t2.write("Este exemplo é muito útil para **manter o programa em execução até que o usuário decida fechá-lo**.")
+
+
+code = '''
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int Numero = 0;
+
+  cout << "Digite um número ou -1 para finalizar: ";
+  cin >> Numero;
+
+  while (Numero != -1)
+  {
+    cout << "Digite um número ou -1 para finalizar: ";
+    cin >> Numero;
+  }
+
+  cout << "FIM" << endl;
+}
+'''
+t2.code(code, language="cpp")
+
+t2.write('Saída:')
+
+t2.text('''
+Digite um número ou -1 para fechar o programa: 3
+Digite um número ou -1 para fechar o programa: 6
+Digite um número ou -1 para fechar o programa: 9
+Digite um número ou -1 para fechar o programa: -1
+FIM
+''')
+
+
+
+
+t2.subheader("Exemplo 3")
+
+t2.write("Neste exemplo, o usuário deverá **digitar um número entre 1 e 3**, pois são os valores de entrada esperados pela aplicação. Caso o **número digitado não esteja neste intervalo**, o **usuário receberá uma mensagem e deverá fazer uma nova entrada de dados**.")
+
+t2.write("Este exemplo é muito útil para **validar as entradas de dados realizadas pelo usuário**, a fim de evitar falhas na aplicação.")
+
+
+code = '''
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int Numero = 0;
+
+  cout << "Digite um número entre 1 e 3: ";
+  cin >> Numero;
+
+  while (Numero < 1 || Numero > 3)
+  {
+    cout << "Número inválido, tente novamente!" << endl;
+
+    cout << "Digite um número entre 1 e 3: ";
+    cin >> Numero;
+  }
+
+  cout << "Parabéns, esse número é válido!" << endl;
+}
+'''
+t2.code(code, language="cpp")
+
+t2.write('Saída:')
+
+t2.text('''
+Digite um número entre 1 e 3: 0
+Número inválido, tente novamente!
+Digite um número entre 1 e 3: 5
+Número inválido, tente novamente!
+Digite um número entre 1 e 3: 1
+Parabéns, esse número é válido!
+''')
+
+
 
 
 
