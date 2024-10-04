b4d2c65d0cab08e8001e9036fda82854b2f8c157
lineulimasjc <lineulimasjc@gmail.com>
2024-10-04 19:53:14 -0300

    Changing backslash to forward slash.

--- app.py
+++ app.py
@@ -10,25 +10,25 @@ st.set_page_config(
 
 pages = {
     "C++ Estruturado": [
-        st.Page("pages\Home.py", title="Home", icon="🏡"), #house_with_garden
-        st.Page("pages\Introducao.py", title="Introdução", icon="📝"), #pencil
-        st.Page("pages\Saida_de_Dados.py", title="Saída de Dados", icon="📊"), #bar_chart
-        st.Page("pages\Variaveis.py", title="Variáveis", icon="🗄️"), #file_cabinet
-        st.Page("pages\Entrada_de_Dados.py", title="Entrada de Dados", icon="⌨️"), #keyboard
-        st.Page("pages\Estrutura_de_Decisao.py", title="Estrutura de Decisão", icon="❓"), #question
-        st.Page("pages\Laco_de_Repeticao.py", title="Laço de Repetição", icon="🔁"), #repeat
-        st.Page("pages\Funcao.py", title="Função", icon="⚙️"), #gear
-        st.Page("pages\Vetor.py", title="Vetor", icon="📏"), #straight_ruler
-        st.Page("pages\Matriz.py", title="Matriz", icon="📋"), #clipboard
-        st.Page("pages\Registro.py", title="Registro", icon="📦"), #package
+        st.Page("pages/Home.py", title="Home", icon="🏡"), #house_with_garden
+        st.Page("pages/Introducao.py", title="Introdução", icon="📝"), #pencil
+        st.Page("pages/Saida_de_Dados.py", title="Saída de Dados", icon="📊"), #bar_chart
+        st.Page("pages/Variaveis.py", title="Variáveis", icon="🗄️"), #file_cabinet
+        st.Page("pages/Entrada_de_Dados.py", title="Entrada de Dados", icon="⌨️"), #keyboard
+        st.Page("pages/Estrutura_de_Decisao.py", title="Estrutura de Decisão", icon="❓"), #question
+        st.Page("pages/Laco_de_Repeticao.py", title="Laço de Repetição", icon="🔁"), #repeat
+        st.Page("pages/Funcao.py", title="Função", icon="⚙️"), #gear
+        st.Page("pages/Vetor.py", title="Vetor", icon="📏"), #straight_ruler
+        st.Page("pages/Matriz.py", title="Matriz", icon="📋"), #clipboard
+        st.Page("pages/Registro.py", title="Registro", icon="📦"), #package
     ],
     # "C++ Orientado a Objetos": [
-    #      st.Page("pages\example_three.py", title="Learn about us"),
-    #      st.Page("pages\example_two.py", title="Try it out"),
+    #      st.Page("pages/example_three.py", title="Learn about us"),
+    #      st.Page("pages/example_two.py", title="Try it out"),
     # ],
     # "Linguagem SQL": [
-    #      st.Page("pages\example_three.py", title="Learn about us"),
-    #      st.Page("pages\example_two.py", title="Try it out"),
+    #      st.Page("pages/example_three.py", title="Learn about us"),
+    #      st.Page("pages/example_two.py", title="Try it out"),
     # ],
 }
 
