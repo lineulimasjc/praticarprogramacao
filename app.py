64bf60a6673c5c6b141d148cc38d0af7b42b554f
lineulimasjc <lineulimasjc@gmail.com>
2024-10-05 09:44:32 -0300

    Updating page title.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Home",
+    page_title="Praticar Programação",
     page_icon="images/logo.png",
     layout="centered",
     initial_sidebar_state="auto",
