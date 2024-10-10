a14009ccd1b83cbbc7ba6de3812040902f1a2650
lineulimasjc <lineulimasjc@gmail.com>
2024-10-10 18:00:53 -0300

    Adding content.

--- pages/Funcao.py
+++ pages/Funcao.py
@@ -1,7 +1,20 @@
 # Função
 import streamlit as st
 
+st.title("O que são funções?")
 
+st.write("Uma **função** é um **bloco de código** que **executa uma determinada operação**. O objetivo da função é **dividir tarefas complexas em tarefas menores**.")
+
+st.write("Uma **função** pode **receber um ou mais parâmetros** e pode também **retornar um valor**.")
+
+st.write("Sintaxe:")
+
+st.text('''
+tipo_do_retorno nomeDaFuncao( lista de parâmetros )
+{
+    // bloco de código
+}
+''')
 
 
 
