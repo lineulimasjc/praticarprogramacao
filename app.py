04736b6834ee5e2b5f6943ff8d203264a26c8701
lineulimasjc <lineulimasjc@gmail.com>
2024-10-18 22:19:37 -0300

    Changed filename.

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ st.set_page_config(
 pgs = {
     "C++ Estruturado": [
         st.Page("pages/home.py", title="Home", icon="🏡"), #house_with_garden
-        st.Page("pages/Introducao.py", title="Introdução", icon="📝"), #pencil
-        st.Page("pages/Saida_de_Dados.py", title="Saída de Dados", icon="📊"), #bar_chart
-        st.Page("pages/Variaveis.py", title="Variáveis", icon="🗄️"), #file_cabinet
-        st.Page("pages/Entrada_de_Dados.py", title="Entrada de Dados", icon="⌨️"), #keyboard
-        st.Page("pages/Estrutura_de_Decisao.py", title="Estrutura de Decisão", icon="❓"), #question
-        st.Page("pages/Laco_de_Repeticao.py", title="Laço de Repetição", icon="🔁"), #repeat
-        st.Page("pages/Funcao.py", title="Função", icon="⚙️"), #gear
-        st.Page("pages/Vetor.py", title="Vetor", icon="📏"), #straight_ruler
-        st.Page("pages/Matriz.py", title="Matriz", icon="📋"), #clipboard
-        st.Page("pages/Registro.py", title="Registro", icon="📦"), #package
+        st.Page("pages/introducao.py", title="Introdução", icon="📝"), #pencil
+        st.Page("pages/saida_de_dados.py", title="Saída de Dados", icon="📊"), #bar_chart
+        st.Page("pages/variaveis.py", title="Variáveis", icon="🗄️"), #file_cabinet
+        st.Page("pages/entrada_de_dados.py", title="Entrada de Dados", icon="⌨️"), #keyboard
+        st.Page("pages/estrutura_de_decisao.py", title="Estrutura de Decisão", icon="❓"), #question
+        st.Page("pages/laco_de_repeticao.py", title="Laço de Repetição", icon="🔁"), #repeat
+        st.Page("pages/funcao.py", title="Função", icon="⚙️"), #gear
+        st.Page("pages/vetor.py", title="Vetor", icon="📏"), #straight_ruler
+        st.Page("pages/matriz.py", title="Matriz", icon="📋"), #clipboard
+        st.Page("pages/registro.py", title="Registro", icon="📦"), #package
     ],
     # "C++ Orientado a Objetos": [
     #      st.Page("pages/example_three.py", title="Learn about us"),
